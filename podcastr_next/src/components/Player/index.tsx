6f3ec564d83d6e6b9c29bc8ac29fb02629b16f57
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-04-20 15:34:15 -0300

    init to create Player component

---
+++ podcastr_next/src/components/Player/index.tsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.scss'
+
+export function Player(){
+
+  return(
+    <div>
+
+    </div>
+  )
+}
--- podcastr_next/src/pages/index.tsx
+++ podcastr_next/src/pages/index.tsx
@@ -3,6 +3,6 @@ import { Header } from "../components/Header"
 
 export default function Home() {
   return (
-    <Header/>
+    <h1>Index</h1>
   )
 }
