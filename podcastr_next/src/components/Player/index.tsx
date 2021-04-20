4be32de63510af5914488877984242d0162db40e
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-04-20 17:45:22 -0300

    styled player and player parts

--- podcastr_next/src/components/Player/index.tsx
+++ podcastr_next/src/components/Player/index.tsx
@@ -4,7 +4,16 @@ export function Player(){
 
   return(
     <div className={styles.playerContainer}>
+      <header>
+        <img src="/playing.svg" alt="PlayingNow Img"/>
+        <strong>Tocando agora</strong>
+      </header>
 
+      <div className={styles.emptyPlayer}>
+        <strong>Selecione um podcast para ouvir</strong>
+      </div>
+
+      <footer></footer>
     </div>
   )
 }
