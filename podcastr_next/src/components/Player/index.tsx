c98b7c06b4ecb22066c1e6c840c39a725fb85cd1
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-04-20 19:55:12 -0300

    player component done

--- podcastr_next/src/components/Player/index.tsx
+++ podcastr_next/src/components/Player/index.tsx
@@ -13,7 +13,33 @@ export function Player(){
         <strong>Selecione um podcast para ouvir</strong>
       </div>
 
-      <footer></footer>
+      <footer className={styles.empty}>
+        <div className={styles.progress}>
+          <span>00:00</span>
+          <div className={styles.slider}>
+          <div className={styles.emptySlider}/>
+          </div>
+          <span>00:00</span>
+        </div>
+
+        <div className={styles.buttons}>
+          <button type="button">
+            <img src="/shuffle.svg" alt="shuffleBtn"/>
+          </button>
+          <button type="button">
+            <img src="/play-previous.svg" alt="previousBtn"/>
+          </button>
+          <button type="button" className={styles.playButton}>
+            <img src="/play.svg" alt="playBtn"/>
+          </button>
+          <button type="button">
+            <img src="/play-next.svg" alt="playNextBtn"/>
+          </button>
+          <button type="button">
+            <img src="/repeat.svg" alt="repeatBtn"/>
+          </button>
+        </div>
+      </footer>
     </div>
   )
 }
