1cdc6ddaf75959fc1a7cfad7bb622703fc31adce
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-04-20 15:08:22 -0300

    created header style

--- podcastr_next/src/components/Header/index.tsx
+++ podcastr_next/src/components/Header/index.tsx
@@ -2,6 +2,8 @@ export function Header(){
   return(
     <header>
       <img src="/logo.svg" alt="PodcastrLogo"/>
+      <p>O melhor para você ouvir, sempre</p>
+      <span>Qui, 8 Abril</span>
     </header>
   )
 }
--- podcastr_next/src/pages/index.tsx
+++ podcastr_next/src/pages/index.tsx
@@ -1,6 +1,8 @@
+import { Header } from "../components/Header"
+
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <Header/>
   )
 }
