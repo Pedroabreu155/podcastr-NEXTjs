9fa3df95a8170b9980c918a95f460d7b23f165a3
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-04-20 21:10:20 -0300

    using SSG to work with APIs

--- podcastr_next/src/pages/index.tsx
+++ podcastr_next/src/pages/index.tsx
@@ -1,8 +1,77 @@
-import { Header } from "../components/Header"
+// import { useEffect } from "react"
+
+//Formas de consumir APIs
 
 
-export default function Home() {
+
+// SPA
+
+// export default function Home() {
+//   //dois parâmetros, oque fazer e quando fazer
+//   //array vazio no react faz que dispare uma unica vez quando carregado
+//   useEffect(() => {
+//     fetch('http://localhost:3333/episodes')
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//   }, [])
+
+
+//   return(
+//     <h1>Index</h1>
+//   )
+// }
+
+// SSR
+// É executado toda vez que alguém acessar a page
+// export default function Home(props){
+//   console.log(props.episodes)
+
+//   return(
+//     <>
+//     <h1>Index</h1>
+//     <p>{JSON.stringify(props.episodes)}</p>
+//     </>
+//   )
+// }
+
+
+// export async function getServerSideProps(){
+//   const response = await fetch('http://localhost:3333/episodes')
+//   const data = await response.json()
+    
+
+//   return {
+//     props: {
+//       episodes: data
+//     }
+//   }
+// }
+
+
+
+// SSG
+
+export default function Home(props){
+  console.log(props.episodes)
+
   return(
+    <>
     <h1>Index</h1>
+    <p>{JSON.stringify(props.episodes)}</p>
+    </>
   )
+}
+
+
+export async function getStaticProps(){
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+    
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8
+  }
 }
